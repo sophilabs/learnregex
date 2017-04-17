f9e7aa4821e19e59d6e1797b768c9712541cfc5f
Pablo Ricco <pricco@gmail.com>
2017-04-16 22:46:29 -0300

    Bump version: 0.3.1 → 0.4.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
 
 setup(
     name='learnregex',
-    version='0.3.1',
+    version='0.4.0',
     description='A pyschool story for learning regular expressions.',
     long_description=readme + '\n\n' + history,
     author='Sophilabs',
