047008db2952f6f3c4d18c61374b95cb7fd7951d
Pablo Ricco <pricco@gmail.com>
2017-04-17 01:00:24 -0300

    Bump version: 0.4.8 → 0.4.9

--- learnregex/__init__.py
+++ learnregex/__init__.py
@@ -8,7 +8,7 @@ from . import (alternation, anchors, capturing, character_classes, greediness,
 
 __author__ = """Sophilabs"""
 __email__ = 'hi@sophilabs.co'
-__version__ = '0.4.8'
+__version__ = '0.4.9'
 
 
 class Story(BaseStory):
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
 
 setup(
     name='learnregex',
-    version='0.4.8',
+    version='0.4.9',
     description='A pyschool story for learning regular expressions.',
     long_description=readme + '\n\n' + history,
     author='Sophilabs',
