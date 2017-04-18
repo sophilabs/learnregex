a32c788ac88de60ce3dd9c6de1c911743c830f05
Pablo Ricco <pricco@gmail.com>
2017-04-18 14:54:24 -0300

    Fix verify and set installer for python >= 3.3.

--- learnregex/utils.py
+++ learnregex/utils.py
@@ -1,6 +1,8 @@
 import importlib
 import inspect
 import random
+import sys
+import os
 
 from story.adventures import AdventureVerificationError
 from story.translation import gettext as _
@@ -8,6 +10,8 @@ from story.translation import gettext as _
 
 def load_solution_function(file, parameters=1):
     try:
+        path, file = os.path.split(os.path.abspath(file))
+        sys.path.append(path)
         if file.endswith('.py'):
             module_name = file[:-3]
         elif file.endswith('.pyc'):
@@ -20,18 +24,18 @@ def load_solution_function(file, parameters=1):
         return function
     except ImportError:
         raise AdventureVerificationError(
-            _("I can't import a module from {}. Are you sure the file exists "
-              "and it's a valid python file?").format(file))
+            _('I can\'t import a module from {}. Are you sure the file exists '
+              'and it\'s a valid python file?').format(file))
     except IndexError:
         raise AdventureVerificationError(
-            _("I can't find a function definition in {}. Read the adventure "
-              "again and use the template proposed there.").format(file))
+            _('I can\'t find a function definition in {}. Read the adventure '
+              'again and use the template proposed there.').format(file))
     except AssertionError:
         raise AdventureVerificationError(
-            _("I found a function in your file but it doesn't accept the "
-              "required number of parameters ({}). If you define several "
-              "functions in your file, make sure the solution function is the "
-              "first one.".format(parameters)))
+            _('I found a function in your file but it doesn\'t accept the '
+              'required number of parameters ({}). If you define several '
+              'functions in your file, make sure the solution function is the '
+              'first one.'.format(parameters)))
 
 
 def get_random_string(dictionary, min_length, max_length):
--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
     install_requires=[
         'story>=1.1.5'
     ],
+    python_requires='!=2, !=3.0.*, !=3.1.*, !=3.2.*, <4',
     license='MIT license',
     zip_safe=False,
     keywords='learnregex',
